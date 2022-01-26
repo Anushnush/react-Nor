1f34c0a742463c6991f4df8873af144b4d511297
Anush <nushyan21@gmail.com>
2022-01-26 14:31:30 +0400

    add all files

---
+++ first-app/src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import './App.css';
+import Nav from './Components/Nav';
+
+function App() {
+
+  return (
+    <>
+      <Nav/>
+    </>
+  );
+}
+export default App;
